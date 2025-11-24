df3f78818b6673eb405e657f9642b47ac691eb94
Henry Aleksanian (Shok.) <henula09106@gmail.com>
2025-11-24 12:50:41 +0400

    refactor: ws auth

--- src/utils/authenticate.ws.ts
+++ src/utils/authenticate.ws.ts
@@ -2,25 +2,11 @@ import { WebSocket } from 'ws';
 import extractToken from './extractToken.js';
 import decodeToken from './decodeToken.js';
 
-const authenticateWs = (authHeader: string, ws: WebSocket) => { // change this without rethrow
-	try {
-		const token = extractToken(authHeader);
-		const userId = decodeToken(token);
+const authenticateWs = (authHeader: string, ws: WebSocket) => {
+	const token = extractToken(authHeader);
+	const userId = decodeToken(token);
 
-		return { userId, token };
-	}
-	catch (error: any) {
-		switch (error.code) {
-			case 'ACCESS_TOKEN_MISSING':
-				ws.close(1008, 'ACCESS_TOKEN_MISSING');
-				break;
-			case 'INVALID_ACCESS_TOKEN':
-				ws.close(1008, 'INVALID_ACCESS_TOKEN');
-				break;
-			default:
-				ws.close(1011, 'INTERNAL_SERVER_ERROR');
-		}
-		return null;
-	}
+	return { userId, token };
 };
+
 export default authenticateWs;
